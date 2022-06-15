6554ea5b7fbace5e79b8c01d9eda1fdbfc943e53
Ricardo Gonçalves <104541413+Riccartt@users.noreply.github.com>
2022-06-15 12:19:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ const answerElement = document.querySelector('#answer')
 const inputQuestion = document.querySelector('#inputQuestion')
 const askButton = document.querySelector('#askButton')
 
-const answers = ['Não', 'Provavelmente', 'Sim']
+const answers = ['Não', 
+                 'Provavelmente', 
+                 'Sim',
+                 'Se concentre e pergunte novamente',
+                 'Não quero estragar a surpresa',
+                 'Pode ter certeza que sim!',
+                 'Pode ter certeza que não!',
+                 'É mais fácil um porco criar asas',
+                 'Nem sonhando...',
+                 'Vejo até coisa melhor no seu futuro',
+                 'Essa está muito difícil de descobrir...',
+                 'Talvez em 2034',
+                 'Isso nem mesmo eu consigo saber']
 
 // Clicar em fazer pergunta
 function makeQuestion() {
